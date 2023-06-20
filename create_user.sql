85bd7e1dde567ac558fc4ff305c7d10005380916
Ignacio Rodríguez <rocha.iarch@gmail.com>
2023-06-19 21:08:43 -0400

    REM: borrar sql de prueba

--- create_user.sql
+++
@@ -1,9 +0,0 @@
-CREATE USER 'adm1n'@'localhost' IDENTIFIED BY 't00r';
-
-GRANT PROCESS ON *.* TO 'adm1n'@'localhost;'
-
-GRANT SELECT, LOCK TABLES ON *.* TO 'adm1n'@'localhost';
-
-GRANT ALL PRIVILEGES ON my_database.* TO 'adm1n'@'localhost';
-
-FLUSH PRIVILEGES;
